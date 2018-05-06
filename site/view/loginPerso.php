3c7c7ee588222d3a80e3e686cb78043d972215d0
Alice Capelle <capelle@localhost.localdomain>
2018-05-06 13:23:56 +0200

    Remove call to model in loginperso view

--- site/view/loginPerso.php
+++ site/view/loginPerso.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
-<?php require("../model/confirm-account.php"); ?>
+<?php require("../controller/confirm-account.php"); ?>
 
 <body>
 	<div class="row fullscreen_height">
