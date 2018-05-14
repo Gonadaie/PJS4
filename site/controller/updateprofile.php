f850eb960052c024c94b22ef6ee34e5b8f5220df
Alice Capelle <acapelle@actioncontrelafaim.org>
2018-05-14 13:38:20 +0200

    Replace way to test post method in updateprofile

--- site/controller/updateprofile.php
+++ site/controller/updateprofile.php
@@ -3,13 +3,20 @@
 require("../model/get_student.php");
 
 
+if (isset($_POST['mail'])) {
+  $student = get_student_by_email($_POST['mail']);
+
+}
+else{
+  $student = get_student_by_id($_SESSION['id']);
+}
 
 /*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $student = get_student_by_email($_POST['mail']);
 
 }
-else{*/
-  $student = get_student_by_id($_SESSION['id']);
+else{
+  $student = get_student_by_id($_SESSION['id']);*/
 
 require("../model/updateprofile.php");
 
