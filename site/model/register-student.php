8c302ebef0824472089330bf709682b429bc05fa
joujoucous <coustenoble.julie@gmail.com>
2018-05-18 11:47:21 +0200

    default description

--- site/model/register-student.php
+++ site/model/register-student.php
@@ -6,12 +6,16 @@ function register_student($student_name, $student_mail, $password_hash, $student
 
 	$db = db_connect();
 	if($db){
-		$query = "INSERT INTO STUDENT (surname, email, password_student, year, pic, score) VALUES (:firstname, :mail, :pass, :year,  '..\images\images_student\alice.png', 500)";
+		$query = "INSERT INTO STUDENT (surname, email, password_student, year, pic, description, score) VALUES (:firstname, :mail, :pass, :year,  '..\images\images_student\alice.png', 'To see a World in a Grain of Sand
+And a Heaven in a Wild Flower 
+Hold Infinity in the palm of your hand 
+And Eternity in an hour - William Blake',500)";
 		$statement = $db->prepare($query);
 		$statement->bindValue(':firstname', $student_name);
 		$statement->bindValue(':mail', $student_mail);
 		$statement->bindValue(':pass', $password_hash);
 		$statement->bindValue(':year', $student_year);
+		
 		$statement->execute();
 
 	}
