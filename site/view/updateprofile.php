c8624f82fb10ba7f280c6356daca162e1e634788
marinnaB <botnari.marina1995@gmail.com>
2018-05-24 11:23:55 +0200

    probleme resolved

--- site/view/updateprofile.php
+++ site/view/updateprofile.php
@@ -27,7 +27,7 @@ $student = $info_student->student;
 		<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 		<!--		<bootstrap>				-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="../styles/bootstrap.min.css" />
         <script src="../scripts/bootstrap.min.js"></script>
 
         <!--		<fav icon>				-->
