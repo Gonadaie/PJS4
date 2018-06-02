14f49c5ceeab65d8c809f408dd124614269528ed
marinnaB <botnari.marina1995@gmail.com>
2018-06-02 18:36:11 +0200

    crop photo name 2

--- site/model/uploadProfilePicture.php
+++ site/model/uploadProfilePicture.php
@@ -26,11 +26,16 @@ if(isset($_POST["image"]))
 
 
 
+// marche pas
+//    $target_dir = "../images/images_student/";
+//    $target_file = $target_dir .str_replace(".", "", "marina.botnari").".png";
+//    file_put_contents($target_file, $data);
+//    echo '<img src="'.$target_file.'" class="img-thumbnail" />';
 
-    $target_dir = "../images/images_student/";
-    $target_file = $target_dir .str_replace(".", "", $student->getEmail()).".png";
-    file_put_contents($target_file, $data);
-    echo '<img src="'.$target_file.'" class="img-thumbnail" />';
+    $imagePath="../images/images_student/";
+    $imageName = $imagePath."test.jpg";
+    file_put_contents($imageName, $data);
+    echo '<img src="'.$imageName.'" class="img-thumbnail" />';
 
 }
 
