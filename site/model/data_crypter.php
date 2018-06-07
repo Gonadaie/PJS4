840e918e8d0f140e7c9dfecb388494e2536656a7
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-06-07 11:48:01 +0200

    add return ton data_crypter

--- site/model/data_crypter.php
+++ site/model/data_crypter.php
@@ -2,13 +2,15 @@
 function encrypt_data($data){
 	$hash = file_get_contents('.key/key');
 	$iv = file_get_contents('.key/vector');
-	$data = openssl_encrypt($data, 'aes-256-cbc', $hash, 0,$iv);
+	$complied_data = openssl_encrypt($data, 'aes-256-cbc', $hash, 0,$iv);
+	return $complied_data;
 }
 
 function decrypt_data($data){
 		$hash = file_get_contents('.key/key');
 		$iv = file_get_contents('.key/vector');
-		$data = openssl_decrypt($DATA, 'aes-256-cbc', $hash,0,$iv);
+		$complied_data = openssl_decrypt($data, 'aes-256-cbc', $hash,0,$iv);
+		return $decrypted_data;
 }
 
 ?>
