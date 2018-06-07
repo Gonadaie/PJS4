386cc61ace7542d2d6d646ea11e20086e6e14bc1
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-06-07 10:13:22 +0200

    get_student modified

--- site/model/get_student.php
+++ site/model/get_student.php
@@ -57,7 +57,7 @@ function get_student_by_id($id){
         FROM ADJECTIVE A, ADJECTIVE A2, ADJECTIVE A3, STUDENT S WHERE S.student_id = :student_id AND S.adjective_1 = A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective";
 
         $statement = $db->prepare($query);
-        $statement->bindValue(':student_id', encrypt($id));
+        $statement->bindValue(':student_id', $id);
         $statement->execute();
 
         $row = $statement->fetch(PDO::FETCH_ASSOC);
