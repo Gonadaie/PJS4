1fa010e6b73938f1f92dbd79e3b262a2e509b231
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-06-07 11:55:03 +0200

    add absolut path

--- site/model/data_crypter.php
+++ site/model/data_crypter.php
@@ -1,14 +1,14 @@
 <?php
 function encrypt_data($data){
-	$hash = file_get_contents('.key/key');
-	$iv = file_get_contents('.key/vector');
+	$hash = file_get_contents('/var/www/html/PJS4/site/model/.key/key');
+	$iv = file_get_contents('/var/www/html/PJS4/site/model/.key/vector');
 	$complied_data = openssl_encrypt($data, 'aes-256-cbc', $hash, 0,$iv);
 	return $complied_data;
 }
 
 function decrypt_data($data){
-		$hash = file_get_contents('.key/key');
-		$iv = file_get_contents('.key/vector');
+		$hash = file_get_contents('/var/www/html/PJS4/site/model/.key/key');
+		$iv = file_get_contents('/var/www/html/PJS4/site/model/.key/vector');
 		$complied_data = openssl_decrypt($data, 'aes-256-cbc', $hash,0,$iv);
 		return $decrypted_data;
 }
