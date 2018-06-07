68d55b596ed95a6811327f54e70bd760bd82172d
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-06-07 11:59:54 +0200

    modify error

--- site/model/data_crypter.php
+++ site/model/data_crypter.php
@@ -9,7 +9,7 @@ function encrypt_data($data){
 function decrypt_data($data){
 		$hash = file_get_contents('/var/www/html/PJS4/site/model/.key/key');
 		$iv = file_get_contents('/var/www/html/PJS4/site/model/.key/vector');
-		$complied_data = openssl_decrypt($data, 'aes-256-cbc', $hash,0,$iv);
+		$decrypted_data = openssl_decrypt($data, 'aes-256-cbc', $hash,0,$iv);
 		return $decrypted_data;
 }
 
