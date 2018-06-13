608d735dea1bed1f8c71079ca0194227d206e468
Alice <alice.capelle.com>
2018-06-13 16:02:45 +0200

    Debug

--- site/model/random_match.php
+++ site/model/random_match.php
@@ -63,7 +63,8 @@ function random_match(){
   else if($nbFirstStudent<$nbSecondStudent){
     while(!empty($unmatched_student_second)){
       for($i=0; $i<$nbFirstStudent; $i++){
-        error_log(print_r(var_dump($unmatched_student_second), TRUE));
+        echo var_dump($unmatched_student_second);
+        //error_log(print_r(var_dump($unmatched_student_second), TRUE));
         insert_random_couple($unmatched_student_second[0][0],$unmatched_student_first[0][$i]);
         array_shift($unmatched_student_second[0]);
         if(empty($unmatched_student_second[0]))
