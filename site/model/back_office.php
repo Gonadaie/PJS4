275761d20b045a59c23c2eb7b8900aae88736a9a
Alice <alice.capelle.com>
2018-06-13 10:21:14 +0200

    Add get couples to controller

--- site/model/back_office.php
+++ site/model/back_office.php
@@ -16,3 +16,22 @@ function get_all_student(){
   error_log(print_r("Requete fini", TRUE));
   return $list;
 }
+
+function get_couples(){
+  $db = db_connect();
+  if($db) {
+    $query = "SELECT S1.email, S2.email FROM student_match as M,
+    student as S1, student as S2 where M.student_id_god_father = S1.student_id and M.student_id_god_son = S2.student_id
+    and M.final = true";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $couples = array();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+      array_push($couples, decrypt_data($row['student_id_god_father']));
+      array_push($couples, decrypt_data($row['student_id_god_son']));
+    }
+  }
+  return $couples;
+}
+
+?>
