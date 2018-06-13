013b74b954f6eef4c66f75d2f95dad4f4cc2fad6
Alice <alice.capelle.com>
2018-06-13 10:53:03 +0200

    Change select in get all student match

--- site/model/back_office.php
+++ site/model/back_office.php
@@ -21,15 +21,15 @@ function get_all_student(){
 function get_couples(){
   $db = db_connect();
   if($db) {
-    $query = "SELECT S1.email, S2.email FROM student_match as M,
+    $query = "SELECT S1.email as email1, S2.email as email2 FROM student_match as M,
     student as S1, student as S2 where M.student_id_god_father = S1.student_id and M.student_id_god_son = S2.student_id
     and M.final = true";
     $statement = $db->prepare($query);
     $statement->execute();
     $couples = array();
     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-      array_push($couples, decrypt_data($row['student_id_god_father']));
-      array_push($couples, decrypt_data($row['student_id_god_son']));
+      array_push($couples, decrypt_data($row['email1']));
+      array_push($couples, decrypt_data($row['email2']));
     }
   }
   return $couples;
