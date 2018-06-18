eaf1f964ca397c24f49732ab8308c025b50537e6
joujoucous <coustenoble.julie@gmail.com>
2018-06-18 13:28:48 +0200

    order the message in previews messages

--- site/model/messaging.php
+++ site/model/messaging.php
@@ -34,9 +34,9 @@ function getPreviewConversation($student_id) {
 	if($db) {
 
 		if(get_year_student($student_id)==1){
-			$query_get_preview_conversation ="SELECT C.conversation_id, C.last_message, S.pic, S.surname, M1.* FROM message M1, conversation C INNER JOIN student S ON C.student_2=S.student_id  WHERE C.student_1=:student_id AND M1.message_id =(SELECT COALESCE(MAX(message_id),'1') FROM message M where M.conversation_id=C.conversation_id ORDER BY C.last_message::DATE desc)";
+			$query_get_preview_conversation ="SELECT C.conversation_id, C.last_message, S.pic, S.surname, M1.* FROM message M1, conversation C INNER JOIN student S ON C.student_2=S.student_id  WHERE C.student_1=:student_id AND M1.message_id =(SELECT COALESCE(MAX(message_id),'1') FROM message M where M.conversation_id=C.conversation_id ORDER BY C.last_message::DATE asc)";
 		}else {
-			$query_get_preview_conversation ="SELECT C.conversation_id, C.last_message, S.pic, S.surname, M1.* FROM message M1, conversation C INNER JOIN student S ON C.student_1=S.student_id  WHERE C.student_2=:student_id AND M1.message_id =(SELECT COALESCE(MAX(message_id),'1') FROM message M where M.conversation_id=C.conversation_id ORDER BY C.last_message::DATE desc)";
+			$query_get_preview_conversation ="SELECT C.conversation_id, C.last_message, S.pic, S.surname, M1.* FROM message M1, conversation C INNER JOIN student S ON C.student_1=S.student_id  WHERE C.student_2=:student_id AND M1.message_id =(SELECT COALESCE(MAX(message_id),'1') FROM message M where M.conversation_id=C.conversation_id ORDER BY C.last_message::DATE asc)";
 		}
 
 		$statement_preview = $db->prepare($query_get_preview_conversation);
