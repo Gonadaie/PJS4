50f29733f43828cb2171516e0be8fb9415029cf3
joujoucous <coustenoble.julie@gmail.com>
2018-06-18 22:44:57 +0200

    test recuperer data ser :hankey:

--- site/scripts/messaging.js
+++ site/scripts/messaging.js
@@ -1,7 +1,14 @@
 const my_list = document.querySelector('.preview_list');
+const message_previews = document.querySelectorAll('.preview_message')
 
 my_list.style.height = window.innerHeight - my_list.offsetTop + "px";
 
 window.addEventListener('resize', () => {
 	my_list.style.height = window.innerHeight - my_list.offsetTop + "px";
 })
+
+for (let i = 0; i < message_previews.length; i++) {
+	message_previews[i].addEventListener('click', () => {
+		console.log(message_previews[i].dataset.student)
+	})
+}
