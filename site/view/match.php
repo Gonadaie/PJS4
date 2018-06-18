b833b00d7d23d61600d1792f6185074fdff46c70
joujoucous <coustenoble.julie@gmail.com>
2018-06-18 18:13:26 +0200

    fix match page

--- site/view/match.php
+++ site/view/match.php
@@ -48,7 +48,7 @@ if(!isset($_SESSION['id']))
 					<div class="col-4">
 						<div class="circle_back">
 							<div class="circle_front">
-								<div class="circle_img"><a href="profile_other_user.php?email=<?= $_POST['mail']?>"><img class="black_and_wite" src="<?= $_POST['pic']?>" alt=""></a></div>
+								<div class="circle_img black_and_wite"><a href="profile_other_user.php?email=<?= $_POST['mail']?>"><img  src="<?= $_POST['pic']?>" alt=""></a></div>
 							</div>
 						</div>
 
