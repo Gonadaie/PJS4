78e272ef3278d52069640b8e64fcb676aa51d498
joujoucous <coustenoble.julie@gmail.com>
2018-06-19 17:45:58 +0200

    :baby:

--- site/scripts/messaging.js
+++ site/scripts/messaging.js
@@ -1,5 +1,7 @@
-const my_list = document.querySelector('.preview_list');
+const my_list = document.querySelector('.preview_list')
 const message_previews = document.querySelectorAll('.preview_message')
+const messaging_welcome_pic = document.querySelectorAll('.messaging_welcome_pic')
+const messaging_conversation = document.querySelectorAll('.messaging_conversation')
 
 my_list.style.height = window.innerHeight - my_list.offsetTop + "px";
 
@@ -10,6 +12,9 @@ window.addEventListener('resize', () => {
 for (let i = 0; i < message_previews.length; i++) {
 	message_previews[i].addEventListener('click', () => {
 		fetch_messages(message_previews[i].dataset.student)
+		messaging_welcome_pic.setAttribute('display', 'none')
+		messaging_conversation.setAttribute('display', 'block')
+
 	})
 }
 
@@ -22,6 +27,7 @@ const fetch_messages = (other_student_id) => {
 		if (this.readyState == 4 && this.status == 200) {
 			var response = this.responseText.replace(/\n/g, "");
 			console.log(this.responseText);
+
 		}
 
 	}
--- site/view/messaging.php
+++ site/view/messaging.php
@@ -35,7 +35,7 @@ require("../controller/messaging.php");
 
 	<body>
 
-		<div class="row fullscreen_height">
+		<div class="row ">
 			<div class=" col-3 no_padding">
 				<div class="menu row ">
 					<div class="offset-1 col-6 my_account_messaging ">
@@ -55,60 +55,26 @@ require("../controller/messaging.php");
 				<div class="right_align title">Messages</div>
 				<div class="scrollable preview_list">
 
-					<?php
-					foreach ($previews as &$preview) {
-						$pic = $preview["pic"];
-						$surname = $preview["surname"];
-						$last_message =  $preview["last_message"];
-						$other_student_id = $preview["other_student_id"];
-						if (mb_strwidth( $preview["message"]["content"])>55){
-							$content =  mb_strimwidth( $preview["message"]["content"], 0, 55, "...");
-						}else $content = $preview["message"]["content"];
-						
-						if ($preview["message"]["flag_read"]==false){
-							$my_preview_div = <<<EOD
-							<div class="row preview_message" data-student="$other_student_id">
-							<div class="offset-1 col-4 left_preview">
-							<div class='notify_circle'></div><img src="$pic" alt=""></div>
-							<div class="col-7 preview_group">
-							<div class="name">$surname : $last_message</div>
-							<div class="preview not_read">$content</div>
-							</div>
-							</div>
-EOD;
-							echo($my_preview_div);
-						} elseif ($preview["message"]["message_id"]==1){
-							$my_preview_div = <<<EOD
-							<div class="row preview_message" data-student="$other_student_id">
-							<div class="offset-1 col-4">
-							<img src="$pic" alt=""></div>
-							<div class="col-7 preview_group">
-							<div class="name">$surname : $last_message</div>
-							<div class="preview no_message_yet">$content</div>
-							</div>
-							</div>
-EOD;
-							echo($my_preview_div);
-						} else 	{
-							$my_preview_div = <<<EOD
-							<div class="row preview_message" data-student="$other_student_id">
-							<div class="offset-1 col-4">
-							<img src="$pic" alt=""></div>
-							<div class="col-7 preview_group">
-							<div class="name">$surname : $last_message</div>
-							<div class="preview ">$content</div>
-							</div>
-							</div>
-EOD;
-							echo($my_preview_div);
-						}
-					} 
-					?>
+
 				</div>
 			</div>
 
 			<div class="col-9 messaging_welcome_pic no_padding">
 				<img src="../images/messaging.png" alt="">
+			</div>
+			<div class="col-9 messaging_conversation no_padding">
+				<div class="conversation_surname">Anissa</div>
+				<div class="conversation_final_btn_off">is it the right one?</div>
+				<div class="conversation_final_btn_on">it's the right one!</div>
+
+
+
+
+				<div class="conversation_messages"></div>
+				<div class="conversation_send_messages row">
+					<div class="col-10 text_zone"><input type="text" name="" id=""></div>
+					<div class="col-1 conversation_send_btn">Envoyer</div>
+				</div>
 			</div>
 		</div>
 		<script src="../scripts/messaging.js"></script>
