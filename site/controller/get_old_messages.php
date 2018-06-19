dc2ad0cb0e1f40fffe19d9b7035489932ebe144f
joujoucous <coustenoble.julie@gmail.com>
2018-06-19 09:21:53 +0200

    test session :+1:

--- site/controller/get_old_messages.php
+++ site/controller/get_old_messages.php
@@ -1,5 +1,9 @@
 <?php 
-$student_id = $_POST['student_id'];
+require("../model/messaging.php");
+
+session_start();
+
+$student_id = $_SESSION['id'];
 $other_student_id = $_POST['other_student_id'];
 
 echo 'mon id est '.$student_id.'mon camarade id est '.$other_student_id;
