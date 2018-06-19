864a814745fd121860f89f330f562f119139bb05
raphael <imgonadaie@gmail.com>
2018-06-19 15:18:57 +0200

    Remove debug logs

--- site/controller/session.php
+++ site/controller/session.php
@@ -2,16 +2,14 @@
 
 $SERVER_ADR = "https://skipti.fr";
 
-error_log(print_r("Am I even called ??", TRUE));
 
 if((!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') || (!empty($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] == 'application/x-www-form-urlencoded')) {
-	error_log(print_r("ici", TRUE));
 }
 else {
 
 	session_start();
 
-	error_log(print_r("Current location : " . $_SERVER['PHP_SELF'], TRUE));
+	//error_log(print_r("Current location : " . $_SERVER['PHP_SELF'], TRUE));
 
 	if(!isset($_SESSION['id'])) {
 
@@ -29,18 +27,12 @@ else {
 		set_include_path($old_include);
 	}
 	else {
-		error_log(print_r("right there bro'", TRUE));
 		$location = $_SERVER['PHP_SELF'];
 		$sploded_location = explode('/', $location);
-		error_log(print_r($sploded_location, TRUE));
 		$page = $sploded_location[count($sploded_location) -1];
-
-		error_log(print_r($page, TRUE));
 
 		if(($page == 'index.php' or $page == 'login.php' or $page == 'signup.php') and in_array("view", $sploded_location))
 			header('Location: ' . $SERVER_ADR . '/view/updateprofile.php');
-	
-		//header("Location: " . $SERVER_ADR . "/view/logout.php");
 	}
 }
 ?>
