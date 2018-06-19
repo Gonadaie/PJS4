f1be92ac9421a5a73182f68c977ec19660dd60d6
joujoucous <coustenoble.julie@gmail.com>
2018-06-19 18:10:09 +0200

    :ambulance:

--- site/scripts/messaging.js
+++ site/scripts/messaging.js
@@ -49,6 +49,6 @@ finalBtnOff.addEventListener('click', () => {
 })
 finalBtnOn.addEventListener('click', () => {
 	finalBtnOn.style.display = 'none'
-	var finalBtnOn = document.querySelector('.conversation_final_btn_on')
+	var finalBtnOff = document.querySelector('.conversation_final_btn_on')
 	finalBtnOff.style.display = 'block'
 })
