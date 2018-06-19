02e268f0695a9e5fa5c1431c838efd2468a9694a
joujoucous <coustenoble.julie@gmail.com>
2018-06-20 00:03:03 +0200

    snb

--- site/scripts/messaging.js
+++ site/scripts/messaging.js
@@ -37,6 +37,7 @@ const fetch_messages = (other_student_id) => {
 		if (this.readyState == 4 && this.status == 200) {
 			var response = new Array();
 			response = JSON.parse(this.responseText);
+			console.log(response)
 			add_messages(response, other_student_id)
 		}
 
