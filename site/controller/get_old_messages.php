edd67ad40c9bdbf974b959f209430eedce77484f
joujoucous <coustenoble.julie@gmail.com>
2018-06-19 11:50:10 +0200

    test

--- site/controller/get_old_messages.php
+++ site/controller/get_old_messages.php
@@ -7,4 +7,7 @@ $student_id = $_SESSION['id'];
 $other_student_id = $_POST['other_student_id'];
 $conv = new Conversation($student_id,$other_student_id);
 $id_conv = $conv->get_id();
-echo get_old_messages($id_conv);
+
+$messages = array();
+$messages = get_old_messages($id_conv);
+die(var_dump($massages));
