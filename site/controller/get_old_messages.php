022cf270ee463ae4b9afaef7517e48c7c3ffc90e
joujoucous <coustenoble.julie@gmail.com>
2018-06-19 11:56:04 +0200

    :crown:

--- site/controller/get_old_messages.php
+++ site/controller/get_old_messages.php
@@ -1,12 +1,12 @@
 <?php 
 require("../model/messaging.php");
+require("../model/chat.php");
 
 session_start();
 
 $student_id = $_SESSION['id'];
 $other_student_id = $_POST['other_student_id'];
-$conv = new Conversation($student_id,$other_student_id);
-$id_conv = $conv->get_id();
+$id_conv = get_id_conversation($student_id,$other_student_id);
 
 $messages = array();
 $messages = get_old_messages($id_conv);
