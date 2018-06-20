770b47558c2d4fc07a015f583dd9a3ab84602c4f
Alice <alice.capelle.com>
2018-06-20 23:14:33 +0200

    Change session to post in insert message

--- site/controller/insert_message.php
+++ site/controller/insert_message.php
@@ -2,7 +2,7 @@
 require_once("../model/messaging.php");
 
 $message = $_POST['content'];
-$sender_id = $_SESSION['id'];
+$sender_id = $_POST['sender'];
 $receiver_id = $_POST['receiver'];
 
 insert_message($message, $sender_id, $receiver_id);
